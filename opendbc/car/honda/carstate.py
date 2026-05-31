443de49a921989a50387918f6be282b3839265e6
MVL <manuel@alumni.virginia.edu>
2026-05-30 21:58:30 -0400

    whitespace fix

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -28,7 +28,7 @@ class CarState(CarStateBase):
       if CP.transmissionType == TransmissionType.cvt:
         self.gearbox_msg = "GEARBOX_CVT"
       self.shifter_values = can_define.dv[self.gearbox_msg]["GEAR_SHIFTER"]
-    
+
     self.car_state_scm_msg = "SCM_FEEDBACK"
     if CP.carFingerprint in HONDA_NIDEC_ALT_SCM_MESSAGES:
       self.car_state_scm_msg = "SCM_BUTTONS"
